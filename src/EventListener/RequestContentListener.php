16ef2ce891633e1d2a405c063879f0d048934343
yusufbulac <ybulac@gmail.com>
2022-02-09 15:22:15 +0300

    Added Content Type Control

--- src/EventListener/RequestContentListener.php
+++ src/EventListener/RequestContentListener.php
@@ -2,7 +2,35 @@
 
 namespace App\EventListener;
 
+use Symfony\Component\HttpFoundation\ParameterBag;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\UnsupportedMediaTypeHttpException;
+
 class RequestContentListener
 {
+    public function onKernelRequest(RequestEvent $event){
+        if(!$event->isMasterRequest()){
+            return;
+        }
 
+        $request = $event->getRequest();
+        $content = $request->getContent();
+
+        if (empty($content)) {
+            $content = '{}';
+        }
+
+        $data = json_decode($content, true);
+
+        if (is_array($data)) {
+            $request->request = new ParameterBag($data);
+        } else {
+            throw new BadRequestHttpException('Invalid JSON data');
+        }
+
+        if($request->headers->get("Content-Type") !== "application/json"){
+            throw new UnsupportedMediaTypeHttpException("Content_Type değeri yanlış");
+        }
+    }
 }
